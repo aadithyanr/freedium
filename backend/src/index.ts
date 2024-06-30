90168d2ca931d00827b523ec8469a845719336b9
Aadithyan Rajesh <nair.aadithyan999@gmail.com>
2024-07-01 01:16:26 +0400

    hono + routes init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,33 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Binding } from 'cloudflare/resources/workers/workers.mjs'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Signup Route')
+
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL, // Corrected typo from datasourceUrl to datasourceUrl
+	}).$extends(withAccelerate())	
+
+	const body = await c.req.json(); 
+	await prisma.user.create({
+		data: {
+			email: body.email,
+			password: body.password,
+		}
+	})
+	return c.text('Signup Route')
 })
 
 app.post('/api/v1/signin', (c) => {
   return c.text('Signin Route')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Signup Route')
 })
 
 app.get('/api/v1/blog/:id', (c) => {
